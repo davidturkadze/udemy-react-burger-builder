7d56d05e648a33efd4b840e6eafebcf661806111
David Turkadze <nebel89@gmail.com>
2021-01-22 10:59:32 +0100

    commit

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -4,7 +4,9 @@ import { NavLink } from 'react-router-dom';
 
 const navigationItem = (props) => (
   <li className={classes.NavigationItem}>
-    <NavLink to={props.link}>{props.children}</NavLink>
+    <NavLink activeClassName={classes.active} to={props.link} exact={props.exact}>
+      {props.children}
+    </NavLink>
   </li>
 );
 
