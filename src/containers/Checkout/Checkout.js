198db2224a956a7a5969f4043e031893600ed99a
David Turkadze <nebel89@gmail.com>
2021-01-31 21:55:52 +0100

    commit

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from './ContactData/ContactData';
@@ -14,9 +14,13 @@ class Checkout extends Component {
   };
 
   render() {
+    let summary = <Redirect to="/" />;
+    if (this.props.ings) {
+      summary = <CheckoutSummary ingredients={this.props.ings} checkoutCancelled={this.checkoutCancelledHandler} checkoutContinued={this.checkoutContinuedHandler} />;
+    }
     return (
       <div>
-        <CheckoutSummary ingredients={this.props.ings} checkoutCancelled={this.checkoutCancelledHandler} checkoutContinued={this.checkoutContinuedHandler} />
+        {summary}
         <Route path={this.props.match.path + '/contact-data'} component={ContactData} />
       </div>
     );
