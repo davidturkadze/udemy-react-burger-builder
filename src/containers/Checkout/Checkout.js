55a9b881dff4e7d803e3f1501af4356728378b6b
David Turkadze <nebel89@gmail.com>
2021-02-02 13:55:46 +0100

    commit

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -15,21 +15,25 @@ class Checkout extends Component {
 
   render() {
     let summary = <Redirect to="/" />;
+
     if (this.props.ings) {
-      summary = <CheckoutSummary ingredients={this.props.ings} checkoutCancelled={this.checkoutCancelledHandler} checkoutContinued={this.checkoutContinuedHandler} />;
+      const purchasedRedirect = this.props.purchased ? <Redirect to="/" /> : null;
+      summary = (
+        <div>
+          {purchasedRedirect}
+          <CheckoutSummary ingredients={this.props.ings} checkoutCancelled={this.checkoutCancelledHandler} checkoutContinued={this.checkoutContinuedHandler} />;
+          <Route path={this.props.match.path + '/contact-data'} component={ContactData} />
+        </div>
+      );
     }
-    return (
-      <div>
-        {summary}
-        <Route path={this.props.match.path + '/contact-data'} component={ContactData} />
-      </div>
-    );
+    return summary;
   }
 }
 
 const mapStateToProps = (state) => {
   return {
-    ings: state.ingredients,
+    ings: state.burgerBuilder.ingredients,
+    purchased: state.order.purchased,
   };
 };
 
