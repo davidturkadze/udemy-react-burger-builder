3ef627039082a2fc3f2aa105d2af717575c511e9
David Turkadze <nebel89@gmail.com>
2021-02-04 13:30:47 +0100

    commit

---
+++ src/store/actions/auth.js
@@ -0,0 +1,47 @@
+import * as actionTypes from './actionTypes';
+import axios from 'axios';
+
+export const authStart = () => {
+  return {
+    type: actionTypes.AUTH_START,
+  };
+};
+
+export const authSuccess = (token, userId) => {
+  return {
+    type: actionTypes.AUTH_SUCCESS,
+    idToken: token,
+    userId: userId,
+  };
+};
+
+export const authFail = (error) => {
+  return {
+    type: actionTypes.AUTH_FAIL,
+    error: error,
+  };
+};
+
+export const auth = (email, password, isSignUp) => {
+  return (dispatch) => {
+    dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true,
+    };
+    let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyANVkMimRG_3I0jNGIYvQEwu4WOwLxc1HY';
+    if (!isSignUp) {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyANVkMimRG_3I0jNGIYvQEwu4WOwLxc1HY';
+    }
+    axios
+      .post(url, authData)
+      .then((response) => {
+        dispatch(authSuccess(response.data.idToken, response.data.localId));
+      })
+      .catch((err) => {
+        console.log(err);
+        dispatch(authFail(err));
+      });
+  };
+};
